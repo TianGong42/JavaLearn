6aa1148cec7b85ace01d7e5c05f07fe385551cc7
czp <375032565@qq.com>
2019-06-22 16:50:20 +0800

    2019年0622提交

--- src/Ten/MyPoint.java
+++ src/Ten/MyPoint.java
@@ -4,5 +4,33 @@ public class MyPoint {
     public double x;
     public double y;
 
+    MyPoint(){
+        x  = 0;
+        y = 0;
+    }
+    MyPoint(double  x,double y){
+        this.x = x;
+        this.y = y;
+    }
+
+
+
+    public double getX(){
+        return this.x;
+    }
+
+    public double getY(){
+        return this.y;
+    }
+
+    public double distance(MyPoint myPoint){
+        return Math.sqrt((this.x - myPoint.getX())* (this.x - myPoint.getX()) + (this.y - myPoint.getY())
+        * (this.y - myPoint.getY()));
+    }
+
+    public double distance(double x,double y){
+        return Math.sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y));
+    }
+
 
 }
